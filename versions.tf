6bf9732df5b878356efc32f96300d6e84e958fea
alamrez <62862526+alamrez@users.noreply.github.com>
2022-07-07 15:08:11 +0100

    blocked out cloud

--- versions.tf
+++ versions.tf
@@ -1,19 +1,19 @@
-terraform {
+# terraform {
 
-  cloud {
-    organization = "hashicorp-learn"
+#   cloud {
+#     organization = "testing83795238749237"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+#     workspaces {
+#       name = "Example-Workspace-1"
+#     }
+#   }
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
-  }
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.28.0"
+#     }
+#   }
 
-  required_version = ">= 0.14.0"
-}
+#   required_version = ">= 0.14.0"
+# }
